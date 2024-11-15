414782f8df4d1f96df1d0995d7870b87a81ee771
manvithakv <manvitha898@gmail.com>
2024-11-15 21:30:42 +0530

    first commit to maven java project

---
+++ src/main/java/com/mavenweb/seproject/App.java
@@ -0,0 +1,10 @@
+package com.mavenweb.seproject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("This is the first Maven Java Project in Eclipse");
+    }
+}
